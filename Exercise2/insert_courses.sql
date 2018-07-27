3563c35df21293831b16b3c9bb66951f6536e767
yaelsalim <yael3000@hotmail.com>
2018-07-26 21:07:33 -0300

    MySql exercises

---
+++ Exercise2/insert_courses.sql
@@ -0,0 +1,33 @@
+INSERT INTO `high-school`.`course`
+(`id_course`,
+`name`,
+`hours_per_week`,
+`assigned_teacher`)
+VALUES
+(1,
+"Maths",
+10,
+2);
+
+INSERT INTO `high-school`.`course`
+(`id_course`,
+`name`,
+`hours_per_week`,
+`assigned_teacher`)
+VALUES
+(2,
+"Physics",
+8,
+1);
+
+INSERT INTO `high-school`.`course`
+(`id_course`,
+`name`,
+`hours_per_week`,
+`assigned_teacher`)
+VALUES
+(3,
+"Literature",
+10,
+3);
+
---
+++ Exercise3/list_students.sql
@@ -0,0 +1,14 @@
+SELECT  c.name AS 'Course: ',
+
+	CONCAT(t.first_name,' ', t.last_name) AS 'Teacher :',
+	
+        CONCAT(s.first_name,' ',s.last_name ) AS 'Student :'
+	
+
+	FROM `high-school`.`course` c JOIN `high-school`.`teacher` t ON (c.assigned_teacher = t.id_teacher)
+   
+        JOIN  `high-school`.`assist_course` atc ON (c.id_course = atc.id_course)
+
+	JOIN `high-school`.`student` s ON (atc.id_student = s.registratrion_number)
+
+	GROUP BY c.name \G;
